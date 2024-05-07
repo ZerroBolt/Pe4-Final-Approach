b5308eaa782259c4175d0d2c224b22626984a7a6
Tycho <tychokerkhofs@gmail.com>
2024-05-07 11:44:10 +0200

    Added very basic player movement

--- GXPEngine2023c/GXPEngine/Player.cs
+++ GXPEngine2023c/GXPEngine/Player.cs
@@ -9,10 +9,49 @@ namespace GXPEngine
     public class Player : AnimationSprite
     {
         Vec2 position;
+        bool arrowKeybind;
 
-        public Player() : base()
+        float velocity = 1f;
+
+        public Player(Vec2 position, bool arrowKeybind = false) : base("square.png", 1, 1)
         {
+            SetOrigin(width / 2, height / 2);
+            this.arrowKeybind = arrowKeybind;
+            this.position = position;
+        }
 
+        void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            if (arrowKeybind)
+            {
+                if (Input.GetKey(Key.LEFT))
+                {
+                    position.SetXY(position.x + -velocity, position.y);
+                }
+                if (Input.GetKey(Key.RIGHT))
+                {
+                    position.SetXY(position.x + velocity, position.y);
+                }
+            }
+            else
+            {
+                if (Input.GetKey(Key.A))
+                {
+                    position.SetXY(position.x + -velocity, position.y);
+                }
+                if (Input.GetKey(Key.D))
+                {
+                    position.SetXY(position.x + velocity, position.y);
+                }
+            }
+
+            this.x = position.x;
+            this.y = position.y;
         }
     }
 }
