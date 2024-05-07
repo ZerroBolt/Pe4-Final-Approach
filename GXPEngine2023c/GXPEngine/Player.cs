6a15946b70a34689dac1cef5dd008854733828ee
Joris <549108@student.saxion.nl>
2024-05-07 10:45:46 +0200

    player
    player

---
+++ GXPEngine2023c/GXPEngine/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    public class Player : AnimationSprite
+    {
+        Vec2 position;
+
+        public Player() : base()
+        {
+
+        }
+    }
+}
