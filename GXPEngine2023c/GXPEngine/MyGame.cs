86f4b319b13a078419d727fc5663ac6218ba66ee
Joris <549108@student.saxion.nl>
2024-05-07 11:40:56 +0200

    data
    data

--- GXPEngine2023c/GXPEngine/MyGame.cs
+++ GXPEngine2023c/GXPEngine/MyGame.cs
@@ -3,9 +3,12 @@ using GXPEngine;
 using System.Drawing;
 
 public class MyGame : Game {
+	public PlayerData playerData;
+
 	public MyGame() : base(800, 600, false)
 	{
-		
+		playerData = new PlayerData();
+
 	}
 
 	void Update() 
---
+++ GXPEngine2023c/GXPEngine/PlayerData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    public class PlayerData
+    {
+        static Vec2 gravityPlayer = new Vec2(0, .1f);
+
+        static float speedPlayer = 10;
+        static float heightJump = 20;
+
+        public Vec2 playerGravity
+        {
+            get
+            {
+                return gravityPlayer;
+            }
+        }
+
+        public float playerSpeed
+        {
+            get
+            {
+                return speedPlayer;
+            }
+        }
+
+        public float jumpHeight
+        {
+            get
+            {
+                return heightJump;
+            }
+        }
+
+        public PlayerData()
+        {
+
+        }
+    }
+}
