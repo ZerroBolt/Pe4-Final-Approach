0f39e07a98f396aeac471d4119887e2c720411af
Joris <549108@student.saxion.nl>
2024-05-13 18:05:24 +0200

    moving collider
    added a movement option for colliders

--- GXPEngine2023c/GXPEngine/LineSegment.cs
+++ GXPEngine2023c/GXPEngine/LineSegment.cs
@@ -17,10 +17,15 @@ namespace GXPEngine
         public uint color;
         public uint lineWidth;
 
+        public bool moving;
+        public float moveSpeed;
+        public float moveTimer;
+        private float moveTimeLeft;
+
         MyGame myGame;
         PlayerData data;
 
-        public LineSegment(Vec2 _start, Vec2 _end)
+        public LineSegment(Vec2 _start, Vec2 _end, bool _moving = false, float _moveSpeed = 0, float _moveTimer = 0)
         {
             myGame = ((MyGame)game);
             data = ((MyGame)game).playerData;
@@ -29,13 +34,35 @@ namespace GXPEngine
             end = _end;
             color = data.lineColor;
             lineWidth = data.lineWidth;
+
+            moving = _moving;
+            moveSpeed = _moveSpeed;
+            moveTimer = _moveTimer;
+            moveTimeLeft = moveTimer;
         }
 
         override protected void RenderSelf(GLContext glContext)
         {
-            if (game != null)
+            if (game != null && !moving)
             {
                 Gizmos.RenderLine(start.x, start.y, end.x, end.y, color, lineWidth);
+            }
+        }
+
+        void Update()
+        {
+            if (game != null && moving)
+            {
+                start.x += moveSpeed;
+                end.x += moveSpeed;
+
+                moveTimeLeft -= Time.deltaTime;
+
+                if (moveTimeLeft <= 0)
+                {
+                    moveSpeed *= -1;
+                    moveTimeLeft = moveTimer;
+                }
             }
         }
     }
