b49929d3db68d4cfa23ed453a524046702f87947
Tycho <tychokerkhofs@gmail.com>
2024-05-14 13:27:09 +0200

    Added basic code for a pulling beam

---
+++ GXPEngine2023c/GXPEngine/PullingBeam.cs
@@ -0,0 +1,45 @@
+﻿using GXPEngine.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    public class PullingBeam : GameObject
+    {
+        Vec2 position = new Vec2(0, 0);
+        Vec2 mousePos = new Vec2(0, 0);
+        public LineSegment line;
+
+        bool firstRun = true;
+        public PullingBeam()
+        {
+            line = new LineSegment(new Vec2(0, 0), new Vec2(0, 0));
+        }
+
+        void Update()
+        {
+            if (firstRun)
+            {
+                parent.AddChild(line);
+                firstRun = false;
+            }
+
+            x = parent.x;
+            y = parent.y;
+
+            //x = position.x = parent.x;
+            //y = position.y = parent.y;
+
+            mousePos = new Vec2(Input.mouseX - x, Input.mouseY - y);
+
+            //position.RotateDegrees((mousePos - position).GetAngleDegrees());
+
+            line.end = mousePos;
+        }
+    }
+}
